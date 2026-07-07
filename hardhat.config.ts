38ca658c1fab4294b53b659c5585b4b2273a9a58
pellyadolfo <pellyadolfo@yahoo.es>
2026-07-07 21:44:56 +0200

    added hardhat-gas-reporter plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -28,7 +29,23 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: "typechain-types",
     target: "ethers-v6"
-  }
+  },
+	gasReporter: {
+		//enabled: process.env.REPORT_GAS !== undefined,
+		enabled: true,                    		// Generate reports when running tests
+		currency: "USD",                  		// Show costs in USD
+		//showMethodSig: true,
+		//showTimeSpent:true,
+		coinmarketcap: process.env.CMC_ID,    // Get price data (optional, free tier available)
+    //etherscan: "your-etherscan-key",  // For L2 gas price data (optional)
+		// gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',		// ethereum
+		// token: 'ETH'
+		// gasPriceApi: 'https://api.bscscan.com/api?module=proxy&action=eth_gasPrice',			// BSC
+		// token: 'BNB'
+		gasPriceApi: 'https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice',		// polygon
+		token: 'MATIC',
+    excludeContracts: ["Migrations"], // Exclude specific contracts
+	},
 };
 
 export default config;
