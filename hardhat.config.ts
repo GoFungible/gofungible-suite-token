bcb8f842de02f9eda89e2fffe0b0b37044dd2d57
pellyadolfo <pellyadolfo@yahoo.es>
2026-08-17 21:43:58 +0200

    test launching nodes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,22 +14,18 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      chainId: 1337
-    },
-    hardhat2: {
-      url: "http://127.0.0.1:8546",
-      chainId: 9999, // Different chain ID
-      accounts: [
-        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",  	// Account 0 - Owner
-        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",		// Account 1
-        "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",		// Account 2
-        "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",		// Account 3
-        "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",		// Account 4
-        "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba"		// Account 5
-      ]
+      chainId: 1337																	// default In-Process Network (does not open a port)
     },
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545"									// default External Network (npx hardhat test)
+    },
+    node1: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1111,
+    },
+    node2: {
+      url: "http://127.0.0.1:8546",
+      chainId: 2222,
     },
 	},
   paths: {
